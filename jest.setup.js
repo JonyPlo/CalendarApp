cd3084da8259e91a01f769113e9915cc14216a09
JonyPlo <joonyyplo@gmail.com>
2023-09-16 22:41:03 -0300

    build: add husky and jest dependencies

---
+++ jest.setup.js
@@ -0,0 +1,7 @@
+import 'whatwg-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.test' });
+jest.mock('./src/helpers/getEnvironments', () => ({
+  getEnvironments: () => ({ ...process.env }),
+}));
