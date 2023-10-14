3350af769656434cf94a04d3afedc0084f9319c8
JonyPlo <joonyyplo@gmail.com>
2023-10-14 09:41:53 -0300

    test(test): :white_check_mark: add more test config and testing axios config
    Add testing in calendarApi file

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,4 +2,8 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFiles: ['./jest.setup.js'],
   transformIgnorePatterns: [],
-};
+
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js',
+  },
+}
