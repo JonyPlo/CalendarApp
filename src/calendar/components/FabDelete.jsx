3f0a3a3254ff41b968185371118852f297553f1f
JonyPlo <joonyyplo@gmail.com>
2023-10-16 17:17:17 -0300

    test(test): :white_check_mark: add FabDelete tests
    Testing in FabDelete component done

--- src/calendar/components/FabDelete.jsx
+++ src/calendar/components/FabDelete.jsx
@@ -4,7 +4,7 @@ export const FabDelete = ({ isDateModalOpen }) => {
   const { startDeletingEvent, activeEvent, hasEventSelected } =
     useCalendarStore()
 
-  const displayBtn = hasEventSelected && !isDateModalOpen ? '' : 'd-none'
+  const displayBtn = hasEventSelected && !isDateModalOpen ? '' : 'd-none2'
 
   const handleDelete = () => {
     startDeletingEvent(activeEvent)
@@ -12,6 +12,7 @@ export const FabDelete = ({ isDateModalOpen }) => {
 
   return (
     <button
+      aria-label='btn-delete'
       className={`btn btn-danger fab-danger ${displayBtn}`}
       onClick={handleDelete}
     >
--- tests/calendar/components/FabDelete.test.jsx
+++ tests/calendar/components/FabDelete.test.jsx
@@ -1,16 +1,52 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { FabDelete } from '../../../src/calendar/components/FabDelete'
-import { Provider } from 'react-redux'
-import { store } from '../../../src/store'
+import { useCalendarStore } from '../../../src/hooks/useCalendarStore'
+
+jest.mock('../../../src/hooks/useCalendarStore')
 
 describe('Testing in <FabDelete/>', () => {
-  test('should display component correctly', () => {
-    render(
-      <Provider store={store}>
-        <FabDelete />
-      </Provider>
-    )
+  const mockStartDeletingEvent = jest.fn()
 
-    screen.debug()
+  beforeEach(() => jest.clearAllMocks())
+
+  test('should display component correctly', () => {
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: false,
+    })
+
+    render(<FabDelete />)
+
+    const btn = screen.getByLabelText('btn-delete')
+
+    expect(btn.classList.toString()).toContain(
+      'btn btn-danger fab-danger d-none'
+    )
+  })
+
+  test('should display the button if there are an active event', () => {
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: true,
+    })
+
+    render(<FabDelete />)
+
+    const btn = screen.getByLabelText('btn-delete')
+
+    expect(btn.classList.toString()).not.toContain('d-none')
+  })
+
+  test('should to call the startDeletingEvent function if there are an active event', () => {
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: true,
+      startDeletingEvent: mockStartDeletingEvent,
+    })
+
+    render(<FabDelete />)
+
+    const btn = screen.getByLabelText('btn-delete')
+
+    fireEvent.click(btn)
+
+    expect(mockStartDeletingEvent).toHaveBeenCalled()
   })
 })
